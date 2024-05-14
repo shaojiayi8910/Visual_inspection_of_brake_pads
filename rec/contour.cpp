0b4d79a441c377ff4bc0de1b6f780ecd24485a84
Shao Jiayi <1069607421@qq.com>
2024-05-14 17:04:53 +0800

    可以测量两个圆心的距离了，羊角的距离打算使用鼠标点击进行测量

--- rec/contour.cpp
+++ rec/contour.cpp
@@ -1,47 +1,28 @@
 #include"contour.h"
 
 
-ContourDetector::ContourDetector(const std::string& filename, double minAreaThreshold) : minArea(minAreaThreshold)
+ContourDetector::ContourDetector(const std::string& filename,double minAreaThreshold)
+:mfilename(filename),minArea(minAreaThreshold)
 {
-    // 读取图像
-    image = cv::imread(filename);
-    if (image.empty())
-    {
-        std::cerr << "Error: Unable to read image file " << filename << std::endl;
-    }
+
 }
 
-cv:: Mat ContourDetector:: detectContours()//检测轮并绘制
+void ContourDetector:: detectContours()//检测轮并绘制
 {
-    // 检查图像是否为空
-    if (image.empty()) {
-        std::cerr << "Error: Image is empty." << std::endl;
-        return cv::Mat();
-    }
-
-    // 将图像转换为灰度图像
-    cv::Mat grayImage;
-    cv::cvtColor(image, grayImage, cv::COLOR_BGR2GRAY);
-    cv::namedWindow("grayImage", cv::WINDOW_NORMAL);
-    cv::imshow("grayImage", grayImage);
-    // 对灰度图像进行阈值处理
-    cv::Mat binaryImage;
-    cv::threshold(grayImage, binaryImage, 140, 255, cv::THRESH_BINARY_INV);
-    cv::namedWindow("binaryImage", cv::WINDOW_NORMAL);
-    cv::imshow("binaryImage", binaryImage);
+     mimage=cv::imread(mfilename);
     // 查找轮廓
+
     std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(binaryImage, contours, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
+    cv::findContours(binaryImage(mfilename), contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
     // 在原始图像上绘制大于阈值的轮廓
     for (size_t i = 0; i < contours.size(); ++i) {
         double area = cv::contourArea(contours[i]);
         if (area >= minArea) {
-            cv::drawContours(image, contours, static_cast<int>(i), cv::Scalar(0, 255, 0), 2);
+            cv::drawContours(mimage, contours, static_cast<int>(i), cv::Scalar(0, 255, 0), 2);
         }
     }
 
-    return image;
 }
 
 
--- rec/contour.h
+++ rec/contour.h
@@ -1,12 +1,13 @@
-#include"ProcessImage.h"
-
-class ContourDetector : public ProcessImage
+#include"basefunc.h"
+class ContourDetector
 {
-private:
+public:
+    const std::string& mfilename;
     double minArea;
+    cv::Mat mimage;
 public:
     ContourDetector(const std::string& filename,double minAreaThreshold);
-    cv::Mat detectContours();
+    void  detectContours();
 };
 
 
