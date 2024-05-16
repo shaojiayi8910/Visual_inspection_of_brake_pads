91ca76ce78070290b4003632db159033d11da107
sunzhonghai <sunzhjl@163.com>
2024-05-16 15:34:09 +0800

    划痕检测

--- rec/main.cpp
+++ rec/main.cpp
@@ -6,6 +6,8 @@
 #include"contour.h"
 #include"circle.h"
 #include "LineDistanceCalculator.h"
+#include "image_processing.hpp"
+
 using namespace std;
 using namespace cv;
 
@@ -24,6 +26,13 @@ int main() {
     //cv::setMouseCallback("Image1", LineDistanceCalculator::onMouse, &calculator);
 
 
+    //圆孔距离的检测
+    cv::Mat image_circle = cv::imread("coumpter_circle.jpg");
+    if (image_circle.empty()) {
+       std::cerr << "Failed to load image  coumpter_circle ! ." << std::endl;
+        return -1;
+    }
+
     // 创建 ContourDetector 对象
     ContourDetector contourDetector("coumpter_circle.jpg",170.00);
     // 检测轮廓
@@ -31,14 +40,6 @@ int main() {
         // 显示包含轮廓的图像
     cv::namedWindow("Contours on Original Image", cv::WINDOW_NORMAL);
    // cv::imshow("Contours on Original Image", contourDetector.mimage);
-
-
-    //圆孔距离的检测
-    cv::Mat image_circle = cv::imread("coumpter_circle.jpg");
-    if (image_circle.empty()) {
-       std::cerr << "Failed to load image  coumpter_circle ! ." << std::endl;
-        return -1;
-    }
 
     // 创建圆检测器对象
     CircleDetector detector;
@@ -64,7 +65,7 @@ int main() {
     //字符识别
     OCRProcessor ocrProcessor("eng");
     std::string recognizedText = ocrProcessor.performOCR(image_original);
-    std::cout << "recognized string: \n" << recognizedText << std::endl;
+    std::cout << "识别的字符: \n" << recognizedText << std::endl;
 
     // 标注识别字符在原图的位置
     ImageAnnotator::annotateImage(image_original, recognizedText);
@@ -76,6 +77,13 @@ int main() {
     //缩小显示字符和铁锈面积窗口的大小
     cv::namedWindow("Result_image", cv::WINDOW_NORMAL);
     cv::resizeWindow("Result_image", image_original.cols / 4, image_original.rows / 4);
+
+    // 读取图像
+    Mat image = imread("Scratch_detection.jpg");
+    Mat color_image = image.clone(); // 复制彩色图像用于绘制直线
+    // 划痕图像处理
+    processImage(image, color_image);
+
 
     //显示原图轮廓
     cv::imshow("Contours on Original Image", contourDetector.mimage);
@@ -89,6 +97,9 @@ int main() {
 
     //在图片上显示字符、铁锈面积和大小
     cv::imshow("Result_image", image_original);
+
+    // 显示划痕
+    imshow("Detected Lines", color_image);
 
     cv::waitKey(0);
     return 0;
